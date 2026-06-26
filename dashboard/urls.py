95b5ad8c313eb186e1adf489d57b39c09b5f4a68
mahmoudalkirm90 <mahmoudalkirm90@gmail.com>
2026-06-27 00:44:39 +0300

    Move view set from dashboard/urls => src/urls

--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers, urlpatterns
-
-from .views import HomeViewSet
-
-router = routers.DefaultRouter()
-
-router.register(r'home', HomeViewSet, basename='home')
-
-urlpatterns = router.urls
